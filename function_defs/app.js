7f1057dcda29b4a1f8e9189526e6f2ed8fe9352c
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-27 09:41:59 +0530

    js functions - methods, constructors, arrows

---
+++ function_defs/app.js
@@ -0,0 +1,37 @@
+// FUNCTIONS AS METHODS
+
+// var person = {
+//     name: 'yugesh',
+//     location: 'Ind',
+//     details: function() {
+//         console.log('Name: '+ this.name + ' ' + 'Location: '+ this.location)
+//         // setInterval(function(){console.log(this)}, 3000);
+//     }
+// };
+
+// person.details();
+// console.log(window.person);
+
+// myDetails = person.details.bind(person);
+
+// myDetails();
+
+// ANONYMOUS FUNCTIONS
+
+// var person = function() {
+//     console.log('this is anonymous function assigned to some variable');
+//     console.log(this);
+// }
+
+// console.log(typeof person);
+// person();
+
+
+//NAMED FUNCTIONS
+
+// function person() {
+//     console.log('this is named function');
+//     console.log(this); // Here this referes to window object
+// }
+
+// person();
