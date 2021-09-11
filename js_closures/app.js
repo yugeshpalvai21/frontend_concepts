86705d3c810418e80134b33b63a523ab6575de29
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-09-11 15:54:48 +0530

    Exporing JS-CLOSURES

--- js_closures/app.js
+++ js_closures/app.js
@@ -1,16 +1,39 @@
+// // WITH Declaration of global variable, manipulation can be done from anywhere
+// var counter = 0;
 
+// function AddCounter() {
+//   counter += 1;
+// }
 
-var addCounter =( function() {
-        let counter = 0;
-        return function() {
-                counter += 1;
+// console.log(counter);
+
+// AddCounter();
+
+// console.log(counter)
+
+// counter = 0;
+
+// console.log(counter);
+
+// WITH CLOSURES DEFINE VARIABLE IN LOCAL SCOPE AND GIVE THAT VARIABLE TO GLOBAL SCOPE
+
+// A closure is a function having access to the parent scope, even after the parent function has closed.
+
+const AddCounter = (() => {
+        var counter = 0;
+        return () => {
+                counter++;
                 return counter;
-            }
-    }
-)();
+        }
+})();
 
-console.log(addCounter());
-console.log(addCounter());
+console.log(AddCounter());
+
+console.log(AddCounter());
+
+console.log(AddCounter());
+
+counter = 0;
 
 console.log(counter);
-
+console.log(AddCounter());
