d4a0202f9e0b59725b838ea66aa7483eab37dac0
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-15 17:11:40 +0530

    dissecting closures

--- 14_js_closures/app.js
+++ 14_js_closures/app.js
@@ -1,39 +1,27 @@
-// // WITH Declaration of global variable, manipulation can be done from anywhere
-// var counter = 0;
+// A closure is created when a function is defined inside another function, allowing the inner function to access variables and parameters of the outer function. 
+// Even after the outer function has finished executing, the inner function retains access to those variables. 
+// This behavior occurs because JavaScript functions create their own scope, and closures allow the inner function to "close over" the outer function’s scope.
 
-// function AddCounter() {
-//   counter += 1;
-// }
+function createPerson(name) {
+        let age = 0; // Private variable
+                
+        return {
+                getName: function() {
+                return name;
+                },
+                getAge: function() {
+                return age;
+                },
+                setAge: function(newAge) {
+                if (newAge >= 0) {
+                age = newAge;
+                }
+                }
+        };
+}
 
-// console.log(counter);
-
-// AddCounter();
-
-// console.log(counter)
-
-// counter = 0;
-
-// console.log(counter);
-
-// WITH CLOSURES DEFINE VARIABLE IN LOCAL SCOPE AND GIVE THAT VARIABLE TO GLOBAL SCOPE
-
-// A closure is a function having access to the parent scope, even after the parent function has closed.
-
-const AddCounter = (() => {
-        var counter = 0;
-        return () => {
-                counter++;
-                return counter;
-        }
-})();
-
-console.log(AddCounter());
-
-console.log(AddCounter());
-
-console.log(AddCounter());
-
-counter = 0;
-
-console.log(counter);
-console.log(AddCounter());
+const person = createPerson('Alice');
+console.log(person.getName()); // Output: Alice
+console.log(person.getAge()); // Output: 0
+person.setAge(30);
+console.log(person.getAge()); // Output: 30
